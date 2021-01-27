a7ebc83e9e5b5a87f4fd43539f6fe95fa031e1bd
Marc J <john.doe.gmail.com>
2021-01-27 14:56:30 +0100

    Security via DB

--- src/main/java/be/hvwebsites/beveiligd/controllers/OffertesController.java
+++ src/main/java/be/hvwebsites/beveiligd/controllers/OffertesController.java
@@ -1,11 +1,13 @@
 package be.hvwebsites.beveiligd.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/offertes")
+@PreAuthorize("hasAuthority('manager')")
 public class OffertesController {
     @GetMapping
     public String offertes(){
--- src/main/java/be/hvwebsites/beveiligd/security/SecurityConfig.java
+++ src/main/java/be/hvwebsites/beveiligd/security/SecurityConfig.java
@@ -1,28 +1,46 @@
 package be.hvwebsites.beveiligd.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String MANAGER= "manager";
     private static final String HELPDESKMEDEWERKER = "helpdeskmedewerker";
     private static final String MAGAZIJNIER = "magazijnier";
 
     @Bean
-    InMemoryUserDetailsManager inMemoryUserDetailsManager(){
-        return new InMemoryUserDetailsManager(
-                User.builder().username("joe").password("{noop}theboss")
-                        .authorities(MANAGER).build(),
-                User.builder().username("averelle").password("{noop}hungry")
-                        .authorities(HELPDESKMEDEWERKER, MAGAZIJNIER).build()
-        );
+    JdbcDaoImpl jdbcDaoImpl(DataSource dataSource){
+        JdbcDaoImpl impl = new JdbcDaoImpl();
+        impl.setDataSource(dataSource);
+        impl.setUsersByUsernameQuery("select naam as username, paswoord as password, actief as enabled" +
+                " from gebruikers where naam = ?");
+        impl.setAuthoritiesByUsernameQuery("select gebruikers.naam as username, rollen.naam as authorities" +
+                " from gebruikers inner join gebruikersrollen" +
+                " on gebruikers.id = gebruikersrollen.gebruikerid" +
+                " inner join rollen" +
+                " on rollen.id = gebruikersrollen.rolid" +
+                " where gebruikers.naam= ?");
+        return impl;
     }
+//    InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+//        return new InMemoryUserDetailsManager(
+//                User.builder().username("joe").password("{noop}theboss")
+//                        .authorities(MANAGER).build(),
+//                User.builder().username("averelle").password("{noop}hungry")
+//                        .authorities(HELPDESKMEDEWERKER, MAGAZIJNIER).build()
+//        );
+//    }
 
     @Override
     public void configure(WebSecurity web){
